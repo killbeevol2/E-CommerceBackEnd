bd1fa27e47622af35eed6be26e5db390fb5562c8
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-08-01 02:47:32 -0700

    finished route to create a new tag in tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -28,10 +28,33 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((tag) => {
+      if (req.body.productIds.length) {
+        const productTagIdArr = req.body.productIds.map((product_id) => {
+          return {
+            tag_id: tag.id,
+            product_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      res.status(200).json(tag);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
 });
 
 router.delete("/:id", (req, res) => {
